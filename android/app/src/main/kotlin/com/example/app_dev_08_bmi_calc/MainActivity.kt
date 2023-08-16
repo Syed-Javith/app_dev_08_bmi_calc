d8015db3a5ad7bad08a3a88930893a8a139ca1fa
Syed Javith <syedjavith14@gmail.com>
2023-08-16 18:35:19 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/app_dev_08_bmi_calc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_dev_08_bmi_calc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/box.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class CustomBMI extends StatelessWidget {
+  const CustomBMI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(20.0),
+
+      ),
+    );
+  }
+}
